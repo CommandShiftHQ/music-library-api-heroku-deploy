31ff32b1e0e37012c12a8e55f05be56c21ef019e
Ersel Aker <erselaker@gmail.com>
2019-06-11 19:47:38 +0100

    make it CORS friendly

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const app = require('./src/app');
+const cors = require('cors');
+
+app.use(cors());
 
 mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true }, () => {
   app.listen(process.env.PORT);
