47f423e4b92a603878944befcc99ba1121f832f4
Ersel Aker <erselaker@gmail.com>
2019-06-11 19:52:04 +0100

    move middleware

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 const app = require('./src/app');
-const cors = require('cors');
-
-app.use(cors());
 
 mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true }, () => {
   app.listen(process.env.PORT);
--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const cors = require('cors');
 const artistRouter = require('./routes/artist');
 const albumRouter = require('./routes/album');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/artists', artistRouter);
